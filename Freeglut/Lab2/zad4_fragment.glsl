a2d5fe18941f287dfab601c6aed1f19fb624522b
msegun <msegun@localhost.localdomain>
2020-03-31 23:38:37 +0200

    Little change in excercise 4

--- Freeglut/Lab2/zad4_fragment.glsl
+++ Freeglut/Lab2/zad4_fragment.glsl
@@ -8,22 +8,26 @@ out vec4 outColor;
 
 void main()
 {
-        vec4 black_t = vec4(0.0, 0.0, 0.0, 0.4);
+        vec4 black_t = vec4(0.0, 0.0, 0.0, 0.0);
+        vec4 black = vec4(0, 0, 0, 0.0);
         vec4 green = vec4(0.0 ,1.0, 0.0, 0.0);
         vec4 pink = vec4(1.0, 0.0, 1.0, 0.0);
-        float alpha = gl_FragCoord.x/500;
+
+        float alpha = gl_FragCoord.y/500;
+        float rev_alpha = 1 - alpha;
+
         if (gl_PrimitiveID % 7 == 0)
-                outColor = mix(black_t, green, alpha);
+                outColor = mix(black, green, alpha);
         if (gl_PrimitiveID % 7 == 1)
-                outColor = mix(black_t, green, 1 - alpha);
+                outColor = mix(black, green, rev_alpha);
         if (gl_PrimitiveID % 7  == 2)
-                outColor = mix(black_t, green, alpha);
+                outColor = mix(black, green, alpha);
         if (gl_PrimitiveID % 7 == 3)
-                outColor = mix(black_t, green, 1 - alpha);
+                outColor = mix(black, green, rev_alpha);
         if (gl_PrimitiveID % 7  == 4)
                 outColor = mix(black_t, pink, alpha);
         if (gl_PrimitiveID % 7 == 5)
-                outColor = mix(black_t, pink, 1 - alpha);
+                outColor = mix(black_t, pink, rev_alpha);
         if (gl_PrimitiveID % 7  == 6)
                 outColor = mix(black_t, pink, alpha);
 }
